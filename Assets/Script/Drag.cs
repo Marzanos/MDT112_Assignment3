4379a83c0c7f73f66e601f39e65a2f45accb9645
Marzanos <punthita.marz@mail.kmutt.ac.th>
2020-04-03 22:50:28 +0700

    In class

---
+++ Assets/Script/Drag.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+
+    public GameObject goalGameobject;
+     private void OnMouseDrag()
+    {
+        Vector3 mousePosWorld3D = Input.mousePosition;
+        mousePosWorld3D.z = transform.position.z - Camera.main.transform.position.z;
+        mousePosWorld3D = Camera.main.ScreenToWorldPoint(mousePosWorld3D);
+
+        transform.position = new Vector3(mousePosWorld3D.x,mousePosWorld3D.y,transform.position.z);
+        
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.Equals(goalGameobject))
+        {
+            Debug.Log("Win");
+        }
+
+    }
+    
+}
